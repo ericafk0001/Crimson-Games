a85cb72acd7577b4f67067b3a4d1d7556a2482e8
Your Name <dxslyofficial@gmail.com>
2023-09-07 21:35:04 -0400

    added basketbros

---
+++ index.js
@@ -0,0 +1,5 @@
+console.warn(
+  "%cNote!",
+  "color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px",
+  "Hello! Please do not steal my website and claim it as your own\nDO NOT INJECT SCRIPTS HERE TOLD BY ANYONE USE AT YOUR OWN RISK"
+);
